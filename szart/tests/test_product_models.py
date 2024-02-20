54bba4b3d58086a31734488d3c24e980693ffeba
maria_sz <kkeraz@gmail.com>
2024-02-20 11:21:41 +0100

    tests product

--- szart/tests/test_product_models.py
+++ szart/tests/test_product_models.py
@@ -1,29 +1,7 @@
-# __init__.py
-# models/
-#     __init__.py
-#     foo.py
-#     bar.py
-# views/
-#     __init__.py
-#     foo.py
-#     bar.py
-# forms/
-#     __init__.py
-#     foo.py
-#     bar.py
-# tests/
-#     __init__.py
-#     test_foo_models.py
-#     test_bar_models.py
-#     test_foo_views.py
-#     test_bar_views.py
-#     test_foo_forms.py
-#     test_bar_forms.py
-
 from django.test import TestCase
 
 from django.contrib.auth.models import User
-from blog.models import Product
+from szartapp.models import Product
 
 class ProductModelTest(TestCase):
     @classmethod
@@ -32,32 +10,58 @@ class ProductModelTest(TestCase):
         Product.objects.create(
             title='Title',
             short_desc='Kiciuś Cat ipsum',
-            author=test_user
-        )
+            price=1200.00)
 
     def test_title_label(self):
         product = Product.objects.get(id=1)
         field_label = product._meta.get_field('title').verbose_name
         self.assertEqual(field_label, 'title')
 
-    def test_short_desc_label(self):
-        product = Product.objects.get(id=1)
-        field_label = product._meta.get_field('short_desc').verbose_name
-        self.assertEqual(field_label, 'short_desc')
-
     def test_title_max_length(self):
         product = Product.objects.get(id=1)
         max_length = product._meta.get_field('title').max_length
         self.assertEqual(max_length, 64)
 
-    def test_object_name_is_title(self):
+    def test_title_blank(self):
         product = Product.objects.get(id=1)
-        expected_object_name = product.title
+        blank = product._meta.get_field('title').blank
+        #self.assertEqual(blank, False)
+        self.assertFalse(blank)
+
+    def test_title_unique(self):
+        product = Product.objects.get(id=1)
+        unique = product._meta.get_field('title').unique
+        self.assertTrue(unique)
+
+
+    def test_short_desc_label(self):
+        product = Product.objects.get(id=1)
+        field_label = product._meta.get_field('short_desc').verbose_name
+        self.assertEqual(field_label, 'short desc')
+
+    def test_short_desc_max_length(self):
+        product = Product.objects.get(id=1)
+        max_length = product._meta.get_field('short_desc').max_length
+        self.assertEqual(max_length, 128)
+
+    def test_short_desc_blank(self):
+        product = Product.objects.get(id=1)
+        blank = product._meta.get_field('short_desc').blank
+        self.assertFalse(blank)
+
+    def test_short_desc_default(self):
+        product = Product.objects.get(id=1)
+        default = product._meta.get_field('short_desc').default
+        self.assertEqual(default, '')
+
+    def test_object_name(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f"{product.id} {product.title} {product.price}"
         self.assertEqual(str(product), expected_object_name)
 
     def test_mark_as_sold(self):
         product = Product.objects.get(id=1)
-        self.assertFalse(product.sold)
+        self.assertEqual(product.sold, 0)
         product.mark_as_sold()
-        self.assertTrue(product.sold)
+        self.assertEqual(product.sold, 1)
 
