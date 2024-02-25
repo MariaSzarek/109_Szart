25cc1caac169ecf10a43af89ac3d746179a6ad01
maria_sz <kkeraz@gmail.com>
2024-02-25 18:01:51 +0100

    update test_product_models

--- szart/tests/test_product_models.py
+++ szart/tests/test_product_models.py
@@ -33,6 +33,20 @@ class ProductModelTest(TestCase):
         unique = product._meta.get_field('title').unique
         self.assertTrue(unique)
 
+    def test_year_label(self):
+        product = Product.objects.get(id=1)
+        field_label = product._meta.get_field('year').verbose_name
+        self.assertEqual(field_label, 'year')
+
+    def test_year_null(self):
+        product = Product.objects.get(id=1)
+        null = product._meta.get_field('year').null
+        self.assertTrue(null)
+
+    def test_year_blank(self):
+        product = Product.objects.get(id=1)
+        blank = product._meta.get_field('year').blank
+        self.assertTrue(blank)
 
     def test_short_desc_label(self):
         product = Product.objects.get(id=1)
@@ -53,6 +67,62 @@ class ProductModelTest(TestCase):
         product = Product.objects.get(id=1)
         default = product._meta.get_field('short_desc').default
         self.assertEqual(default, '')
+
+    def test_description_label(self):
+        product = Product.objects.get(id=1)
+        field_label = product._meta.get_field('description').verbose_name
+        self.assertEqual(field_label, 'description')
+
+    def test_description_default(self):
+        product = Product.objects.get(id=1)
+        default = product._meta.get_field('description').default
+        self.assertEqual(default, '')
+
+
+    def test_type_label(self):
+        product = Product.objects.get(id=1)
+        type = product._meta.get_field('type').verbose_name
+        self.assertEqual(type, 'type')
+
+    def test_type_default(self):
+        product = Product.objects.get(id=1)
+        default = product._meta.get_field('type').default
+        self.assertEqual(default, 0)
+
+    def test_type_choices(self):
+        product = Product.objects.get(id=1)
+        default = product._meta.get_field('type').choices
+        self.assertEqual(default, product.TYPE)
+
+    def test_price_label(self):
+        product = Product.objects.get(id=1)
+        type = product._meta.get_field('price').verbose_name
+        self.assertEqual(type, 'price')
+
+    def test_price_max_digits(self):
+        product = Product.objects.get(id=1)
+        max_digits = product._meta.get_field('price').max_digits
+        self.assertEqual(max_digits, 7)
+
+    def test_price_decimal_places(self):
+        product = Product.objects.get(id=1)
+        decimal_places = product._meta.get_field('price').decimal_places
+        self.assertEqual(decimal_places, 2)
+
+    def test_sold_label(self):
+        product = Product.objects.get(id=1)
+        field_label = product._meta.get_field('sold').verbose_name
+        self.assertEqual(field_label, 'sold')
+
+    def test_sold_default(self):
+        product = Product.objects.get(id=1)
+        default = product._meta.get_field('sold').default
+        self.assertEqual(default, 0)
+
+    def test_sold_choices(self):
+        product = Product.objects.get(id=1)
+        choices = product._meta.get_field('sold').choices
+        self.assertEqual(choices, product.SOLD)
 
     def test_object_name(self):
         product = Product.objects.get(id=1)
