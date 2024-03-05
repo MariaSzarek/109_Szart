4cc422272f1a1575987db3fbae5e3b2dbdcee090
maria_sz <kkeraz@gmail.com>
2024-03-05 10:28:12 +0100

    test_forms update

--- szart/tests/test_forms.py
+++ szart/tests/test_forms.py
@@ -7,15 +7,15 @@ class MessageFormTest(TestCase):
         # Properties of the form fields
         message_form = MessageForm({
             'nadawca': 'jakis_user',
-            'email': 'address',
+            'email': '@address',
             'temat': 'subject',
             'tresc' : 'user_message'
         })
 
-        # self.assertEqual(message_form.fields['title'].widget.input_type, 'text')
+        # print(message_form.fields) - spr typu fielda
         self.assertTrue(isinstance(message_form.fields['nadawca'], forms.CharField))
         self.assertTrue(isinstance(message_form.fields['email'], forms.EmailField))
         self.assertTrue(isinstance(message_form.fields['temat'], forms.CharField))
-        # self.assertTrue(isinstance(message_form.fields['tresc'], forms.TextField))
+        self.assertTrue(isinstance(message_form.fields['tresc'], forms.CharField))
 
 
