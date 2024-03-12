a1827553b9f79287e40b697a445ab642667bfd33
maria_sz <kkeraz@gmail.com>
2024-03-12 22:28:53 +0100

    updated test_views.py 1 test działa

--- szart/tests/test_views.py
+++ szart/tests/test_views.py
@@ -13,22 +13,34 @@ from szartapp.views import index
 class ViewsTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
-        self.post = Product.objects.create(title='Product1', short_desc='This is a short desc', price=1200.00)
+        self.product = Product.objects.create(title='Product1', short_desc='This is a short desc', price=1200.00)
 
-    def test_index(self):
-        with CaptureQueriesContext(connection) as ctx:
-            response = self.client.get('index')
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'Szart.html')
+    # def test_index(self):
+    #     with CaptureQueriesContext(connection) as ctx:
+    #         response = self.client.get(reverse('szart'))
+    #         self.assertEqual(response.status_code, 200)
+    #         self.assertTemplateUsed(response, 'Szart.html')
+    #
+    #         # expected_query = (
+    #         #     f"""SELECT "szartapp_product"."id", "szartapp_product"."title", "szartapp_product"."year",
+    #         #     "szartapp_product"."short_desc", "szartapp_product"."description", "szartapp_product"."image",
+    #         #     "szartapp_product"."type", "szartapp_product"."price", "szartapp_product"."created",
+    #         #     "szartapp_product"."sold"
+    #         #     FROM "szartapp_product"
+    #         #     ORDER BY "szartapp_product"."id" ASC"""
+    #
+    #         expected_query = (
+    #             f"""SELECT "szartapp_product"."id", "szartapp_product"."title", "szartapp_product"."year",
+    #             "szartapp_product"."short_desc", "szartapp_product"."description", "szartapp_product"."image",
+    #             "szartapp_product"."type", "szartapp_product"."price", "szartapp_product"."created",
+    #             "szartapp_product"."sold"
+    #             FROM "szartapp_product" """
+    #         ).replace("\n", "").replace(" ", "")
+    #         actual_query = ctx.captured_queries[0]['sql'].replace("\n", "").replace(" ", "")
+    #         self.maxDiff = None
+    #         self.assertEqual(expected_query, actual_query)
 
-            # # Check the SQL query generated
-            # expected_query = (
-            #     f"""SELECT "blog_post"."id", "blog_post"."author_id", "blog_post"."title",
-            #     "blog_post"."text", "blog_post"."created_date", "blog_post"."published_date"
-            #     FROM "blog_post"
-            #     WHERE "blog_post"."published_date" <= \'2024-03-05 08:05:21.118404\'
-            #     ORDER BY "blog_post"."published_date" ASC """
-            # ).replace("\n", "").replace(" ", "")
-            #
-            # actual_query = ctx.captured_queries[0]['sql'].replace("\n", "").replace(" ", "")
-            # self.assertEqual(expected_query, actual_query)
+    def test_item_response(self):
+        response = self.client.get(reverse('item', kwargs={'id': self.product.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'item.html')
